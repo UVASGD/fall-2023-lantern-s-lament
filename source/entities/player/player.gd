c5f0c40b1d543f5852663a31ac16819ee0a1c6da
stick-h <68358227+stick-h@users.noreply.github.com>
2023-10-28 15:01:09 -0400

    fixed thingy

--- source/entities/player/player.gd
+++ source/entities/player/player.gd
@@ -51,36 +51,10 @@ const CENTER = Vector2(0.0, 0.0)
 
 func _ready():
 	setup_stats()
-	animation_tree.set("parameters/Idle/blend_position", Vector2(0, 1))
 	game_start = true
 
 func _physics_process(delta):
 	back_angle = man_aim_angle + PI
-<<<<<<< Updated upstream
-	light_strength = 0.5+cur_hp/100.0
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-	
-	queue_redraw()
-	
-	direction = Vector2(
-		Input.get_action_strength("rotate_right") - Input.get_action_strength("rotate_left"),
-		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	)
-	
-	speed += direction*50
-	speed *= 0.9
-	move(speed)
-<<<<<<< Updated upstream
-	update_animation()
-=======
-
-	if Input.is_action_pressed("rotate_left") and !Input.is_action_pressed("rotate_right"):
-		speed += Vector2(-50, 0)
-	elif Input.is_action_pressed("rotate_right") and !Input.is_action_pressed("rotate_left"):
-		speed += Vector2(50, 0)
-=======
 	light_strength = 0.5+cur_hp/200.0
 	
 	queue_redraw()
@@ -97,8 +71,6 @@ func _physics_process(delta):
 	update_animation()
 	
 	# writing a script into a two-dimensional way of doing this
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
 	looker2(get_global_mouse_position(), delta)
 
