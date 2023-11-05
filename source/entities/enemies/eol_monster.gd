70c1fde459ce44d80c3284a53f85b9505e62c09e
viamill <126583359+viamill@users.noreply.github.com>
2023-11-05 17:39:58 -0500

    torches and light

--- source/entities/enemies/eol_monster.gd
+++ source/entities/enemies/eol_monster.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var player = get_tree().get_root().get_node("MainScene/Player")
 @onready var speed = 5
-@onready var damage = 5
+@onready var damage = 50
 @onready var boost = 0
 
 func _ready():
