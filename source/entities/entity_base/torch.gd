787bd13e2ced2d2de76ab76bbbd855b193624297
kedrickfudala <KedrickAFudala@gmail.com>
2023-11-19 15:37:16 -0500

    y sort fixes

--- source/entities/entity_base/torch.gd
+++ source/entities/entity_base/torch.gd
@@ -4,14 +4,8 @@ extends Area2D
 @onready var lit = false
 @onready var torch = get_tree().get_root().get_node("MainScene/FinalTorch")
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+	z_index = 1
 
 func _on_area_entered(_area):
 	if !lit:
